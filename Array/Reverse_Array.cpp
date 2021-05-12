0a672c05b1969a7783990c4007268e3c5cc8f6c0
Halge Sakshi <47245574+Asakshi@users.noreply.github.com>
2021-05-12 21:24:00 +0530

    Reversing an array by using function

---
+++ Array/Reverse_Array.cpp
@@ -0,0 +1,34 @@
+//
+
+#include <iostream>
+
+int main()
+{
+    int a[] = { 1,2,3,4,5,6 };
+
+    std::cout << "Before: ";
+    for (int ele : a)
+    {
+        std::cout << ele << " ";
+    }
+
+    int first_index = 0;
+    int last_index = (sizeof(a) / sizeof(a[0])) - 1;
+
+    while (first_index < last_index)
+    {
+        int temp;
+        temp = a[first_index];
+        a[first_index] = a[last_index];
+        a[last_index] = temp;
+        first_index++;
+        last_index--;
+    }
+
+    std::cout << "\nAfter: ";
+    for (int ele : a)
+    {
+        std::cout << ele << " ";
+    }
+}
+
