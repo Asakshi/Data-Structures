8b9677804cea316a912f1ea89452aa32d711a55b
Halge Sakshi <47245574+Asakshi@users.noreply.github.com>
2021-05-20 09:20:25 +0530

    Update Reverse_Array_STL.cpp

--- Array/Reverse_Array_STL.cpp
+++ Array/Reverse_Array_STL.cpp
@@ -1,4 +1,29 @@
 #include<iostream>
 #include<algorithm>
 
+void printArray(int arr[])
+{
+    for (int ele : arr)
+    {
+        std::cout << ele << " ";
+    }
+    
+    std::cout<<'\n';
+}
 
+//Driver function
+int main()
+{
+    int arr[] = { 1,2,3,4,5,6 };
+
+    std::cout << "Before Reversing Array: ";
+    printArray(arr);
+    
+    int size = (sizeof(arr) / sizeof(arr[0]));
+    std::reverse(arr, arr+size);
+    
+    std::cout << "\nAfter Reversing Array:";
+    printArray(arr);
+    
+   return 0;
+}
