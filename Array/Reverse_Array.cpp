0bfc0780caaf4123df1406aae2bef6a3cfa5267b
Halge Sakshi <47245574+Asakshi@users.noreply.github.com>
2021-05-20 09:12:51 +0530

    Update Reverse_Array.cpp

--- Array/Reverse_Array.cpp
+++ Array/Reverse_Array.cpp
@@ -2,20 +2,11 @@
 
 #include <bits/stdc++.h>
 
-int main()
+void reverseArrar(int arr[], int size)
 {
-    int a[] = { 1,2,3,4,5,6 };
-
-    std::cout << "Before: ";
-    for (int ele : a)
-    {
-        std::cout << ele << " ";
-    }
-
-    int first_index = 0;
-    int last_index = (sizeof(a) / sizeof(a[0])) - 1;
-
-    while (first_index < last_index)
+    int first_indxe = 0;
+    int last_index = size-1;
+     while (first_index < last_index)
     {
         int temp;
         temp = a[first_index];
@@ -24,11 +15,32 @@ int main()
         first_index++;
         last_index--;
     }
+}
 
-    std::cout << "\nAfter: ";
-    for (int ele : a)
+void printArray(int arr[])
+{
+    for (int ele : arr)
     {
         std::cout << ele << " ";
     }
+    
+    std::cout<<'\n';
+}
+
+//Driver function
+int main()
+{
+    int arr[] = { 1,2,3,4,5,6 };
+
+    std::cout << "Before Reversing Array: ";
+    printArray(arr);
+    
+    int size = (sizeof(arr) / sizeof(arr[0]));
+    reverseArray(arr, size);
+    
+    std::cout << "\nAfter Reversing Array:";
+    printArray(arr);
+    
+   return 0;
 }
 
